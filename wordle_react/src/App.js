9b925085f777499d53e80928133ef738324ec61a
M9Bene <bene9marci@gmail.com>
2023-02-03 16:20:26 +0100

    Add: game territory and bordered space for guesses

--- wordle_react/src/App.js
+++ wordle_react/src/App.js
@@ -3,9 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className={"Header"}>
+      <div className={"header"}>
         <p>WORDLE</p>
       </div>
+        <div className={"game-territory"}>
+            <div className={"guesses"}>
+
+            </div>
+        </div>
     </div>
   );
 }
