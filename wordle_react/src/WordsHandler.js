38f4af9456f4f8cc3309c2118bf74304fe2850e1
M9Bene <bene9marci@gmail.com>
2023-04-05 18:01:03 +0200

    Add: default secret word

--- wordle_react/src/WordsHandler.js
+++ wordle_react/src/WordsHandler.js
@@ -7,7 +7,10 @@ export function getEmptyGuesses() {
 
 export function getSecretWord() {
 
-    let secretWord = words[Math.trunc(Math.random() * words.length)].toUpperCase();
+   // let secretWord = words[Math.trunc(Math.random() * words.length)].toUpperCase();
+
+    let secretWord = "EARTH";
+
     return secretWord.split("");
 }
 
